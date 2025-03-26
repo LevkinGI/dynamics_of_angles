a143c97b77fc00c8e95aef92f6381579f13c4d64
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-03-26 09:37:53 +0300

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -15,7 +15,8 @@ def create_phi_fig(time, phi, phi_fit, H, T, approx_freqs_GHz, theor_freqs_GHz):
             "x": 0.95,
             "y": 1.05,
             "text": (
-                f"<b>Магнитное поле</b> H = {H} Oe<br>"
+                f"<b>Материал</b> {'FeFe' if material == '1' else 'GdFe'}<br>"
+                f"<b>Магнитное поле</b> H = {H} Э<br>"
                 f"<b>Температура</b> T = {T} K<br>"
                 f"<b>HF</b> Аппроксимация: {approx_freqs_GHz[0]} ГГц; Аналитика: {theor_freqs_GHz[0]} ГГц<br>"
                 f"<b>LF</b> Аппроксимация: {approx_freqs_GHz[1]} ГГц; Аналитика: {theor_freqs_GHz[1]} ГГц"
