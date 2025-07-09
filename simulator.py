59b6df87fc4fb3743eea0b9aa2a0534c88580bca
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-07-09 16:09:13 +0300

    Подгоняем теорию

--- simulator.py
+++ simulator.py
@@ -3,9 +3,10 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from numba import njit
 from constants import gamma, h_IFE, delta_t
+from config import SimParams
 
 @njit(fastmath=True)
-def dynamics(t, y, H, T, m, M, chi, K, kappa, gamma, alpha):
+def dynamics(t, y, H, kappa, K, chi, m, M, alpha):
     theta, phi, dtheta, dphi = y
     a = alpha * M * gamma / chi
     b = (np.abs(m) * gamma**2 * H / chi - gamma**2 * H**2 + 2 * K * gamma**2 / chi)
@@ -19,24 +20,43 @@ def dynamics(t, y, H, T, m, M, chi, K, kappa, gamma, alpha):
         ddphi = -a * dphi - b * phi - c * dtheta
     return [dtheta, dphi, ddtheta, ddphi]
 
-def run_simulation(H, T, m, M, chi, K, kappa, alpha, simulation_time=0.3e-9, num_points=1001,
-                   method='DOP853', rtol=1e-10, atol=1e-12):
+def run_simulation(
+        H_val: float,
+        T_val: float,
+        base_m: float,
+        base_M: float,
+        base_K: float,
+        params: SimParams,
+        t_max: float = simulation_time,
+        num_points: int = 1001,
+        method: str = 'DOP853',
+        rtol: float = 1e-10,
+        atol: float = 1e-12,
+):
+    # Применяем коэффициенты для подгона теории
+    m_val   = params.m_scale * base_m
+    M_val   = params.M_scale * base_M
+    K_val   = params.k_scale * base_K
+    chi_val = params.chi
+    alpha   = params.alpha
+    kappa   = m_val / gamma
+
     # Начальные условия (в радианах и рад/с)
     theta_initial = 0.0
     phi_initial = 0.0
     dtheta_initial = 0.0
-    dphi_initial = gamma**2 * (H + np.abs(m)/chi) * h_IFE * delta_t
+    dphi_initial = (gamma**2) * (H_val + abs(m_val) / chi_val) * h_IFE * delta_t
                        
     y0 = [theta_initial, phi_initial, dtheta_initial, dphi_initial]
     t_eval = np.linspace(0, simulation_time, num_points)
     solution = solve_ivp(
         dynamics,
-        (0, simulation_time),
+        (0.0, t_max),
         y0,
-        args=(H, T, m, M, chi, K, kappa, gamma, alpha),
         t_eval=t_eval,
+        args=(H_val, kappa, K_val, chi_val, m_val, M_val, alpha),
         method=method,
         rtol=rtol,
-        atol=atol
+        atol=atol,
     )
     return solution.t, solution.y
