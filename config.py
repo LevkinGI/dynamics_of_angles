2c578b96de089098f4b7929cbfb5ea5720632557
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-07-09 19:45:01 +0300

    Подгоняем теорию

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class SimParams:
-    alpha: float   # коэффициент затухания
-    chi:   float   # магнитная восприимчивость (теперь скаляр)
+    alpha_scale: float   # множитель для коэффициента затухания α
+    chi_scale:   float   # множитель для магнитной восприимчивости χ (теперь скаляр)
     k_scale:  float  # множитель для K(T)
     m_scale: float  # множитель массива m
     M_scale: float  # множитель массива M
