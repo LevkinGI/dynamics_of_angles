8c2df2e67032128cd891546cd6bf7b1273305464
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-07-11 12:52:57 +0300

    Подгоняем теорию

--- app.py
+++ app.py
@@ -415,7 +415,7 @@ def update_graphs(store, freqs, H, T, material):
     
     theor_freqs_GHz = sorted(np.round([freq_array1[t_index, h_index], freq_array2[t_index, h_index]], 1), reverse=True)
 
-    sim_time, sol = run_simulation(H, T, m_val, M_val, K_val, chi_val, alpha, kappa)
+    sim_time, sol = run_simulation(H, m_val, M_val, K_val, chi_val, alpha, kappa)
 
     time_ns = sim_time * 1e9
     theta = np.degrees(sol[0])
