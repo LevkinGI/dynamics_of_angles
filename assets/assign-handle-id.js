de8863f9639cf2e0abe7d627c3c0d03fd49bec4f
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-07-11 10:54:14 +0300

    Update assign-handle-id.js

--- assets/assign-handle-id.js
+++ assets/assign-handle-id.js
@@ -1,18 +1,18 @@
-// assign-handle-id.js
-// Назначаем каждому бегунку (ручке) id вида "<id-слайдера>-handle",
-// чтобы на него можно было повесить dbc.Tooltip.
+// // assign-handle-id.js
+// // Назначаем каждому бегунку (ручке) id вида "<id-слайдера>-handle",
+// // чтобы на него можно было повесить dbc.Tooltip.
 
-document.addEventListener("DOMContentLoaded", () => {
-  // 1. Берём ВСЕ элементы-бегунки, которые создаёт rc-slider.
-  document.querySelectorAll(".rc-slider-handle").forEach(handle => {
+// document.addEventListener("DOMContentLoaded", () => {
+//   // 1. Берём ВСЕ элементы-бегунки, которые создаёт rc-slider.
+//   document.querySelectorAll(".rc-slider-handle").forEach(handle => {
 
-    // 2. Поднимаемся вверх по дереву и ищем ближайший <div>, у которого есть id —
-    //    это тот самый контейнер, который Dash сгенерировал на основе
-    //    dcc.Slider(id="...").
-    const sliderDiv = handle.closest("div[id]");   // <div id="alpha-scale-slider">…<div class="rc-slider">…
-    if (!sliderDiv) return;                        // на случай экзотической вёрстки
+//     // 2. Поднимаемся вверх по дереву и ищем ближайший <div>, у которого есть id —
+//     //    это тот самый контейнер, который Dash сгенерировал на основе
+//     //    dcc.Slider(id="...").
+//     const sliderDiv = handle.closest("div[id]");   // <div id="alpha-scale-slider">…<div class="rc-slider">…
+//     if (!sliderDiv) return;                        // на случай экзотической вёрстки
 
-    // 3. Формируем новый id для ручки и присваиваем.
-    handle.id = `${sliderDiv.id}-handle`;          // получаем alpha-scale-slider-handle
-  });
-});
+//     // 3. Формируем новый id для ручки и присваиваем.
+//     handle.id = `${sliderDiv.id}-handle`;          // получаем alpha-scale-slider-handle
+//   });
+// });
