ec9172e01890117b2fe8a65dedd7799e1806670e
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-07-16 17:50:14 +0300

    Загоняем fix_graphs в update_graphs

--- app.py
+++ app.py
@@ -308,37 +308,37 @@ def move_m_slider(logk):
     k = 10**logk
     return f"{k:.2f} × m"
 
-@app.callback(
-    [Output('H_fix-graph', 'figure'),
-     Output('T_fix-graph', 'figure')],
-    [Input('H-slider',  'value'),
-    Input('T-slider',  'value'),
-    Input('param-store', 'data'),
-    Input('material-dropdown', 'value')]
-)
-def live_fix_graphs(H, T, store, material):
-    p0 = SimParams(**store[material])
+# @app.callback(
+#     [Output('H_fix-graph', 'figure'),
+#      Output('T_fix-graph', 'figure')],
+#     [Input('H-slider',  'value'),
+#     Input('T-slider',  'value'),
+#     Input('param-store', 'data'),
+#     Input('material-dropdown', 'value')]
+# )
+# def live_fix_graphs(H, T, store, material):
+#     p0 = SimParams(**store[material])
 
-    T_vals    = T_vals_1 if material == '1' else T_vals_2
-    t_index   = np.abs(T_vals - T).argmin()
-    m_vec_T   = p0.m_scale * (m_array_1 if material == '1' else m_array_2)
-    K_vec_T   = p0.k_scale * (K_array_1 if material == '1' else K_array_2)
-    chi_vec_T = p0.chi_scale * (chi_array_1 if material == '1' else chi_array_2)
+#     T_vals    = T_vals_1 if material == '1' else T_vals_2
+#     t_index   = np.abs(T_vals - T).argmin()
+#     m_vec_T   = p0.m_scale * (m_array_1 if material == '1' else m_array_2)
+#     K_vec_T   = p0.k_scale * (K_array_1 if material == '1' else K_array_2)
+#     chi_vec_T = p0.chi_scale * (chi_array_1 if material == '1' else chi_array_2)
 
-    m_T   = m_vec_T[t_index]
-    K_T   = K_vec_T[t_index]
-    chi_T = chi_vec_T[t_index]
+#     m_T   = m_vec_T[t_index]
+#     K_T   = K_vec_T[t_index]
+#     chi_T = chi_vec_T[t_index]
 
-    f1_T, f2_T = compute_frequencies_H_fix(H, m_vec_T, chi_vec_T, K_vec_T, gamma)
-    f1_H, f2_H = compute_frequencies_T_fix(H_vals, m_T, chi_T, K_T, gamma)
+#     f1_T, f2_T = compute_frequencies_H_fix(H, m_vec_T, chi_vec_T, K_vec_T, gamma)
+#     f1_H, f2_H = compute_frequencies_T_fix(H_vals, m_T, chi_T, K_T, gamma)
 
-    H_data = H_1000 if H==1000 and material == '1' else None
-    T_data = T_293 if T==293 and material == '1' else None
+#     H_data = H_1000 if H==1000 and material == '1' else None
+#     T_data = T_293 if T==293 and material == '1' else None
 
-    H_fix_fig = create_H_fix_fig(T_vals, (f1_T, f2_T), H, H_data)
-    T_fix_fig = create_T_fix_fig(H_vals, (f1_H, f2_H), T, T_data)
+#     H_fix_fig = create_H_fix_fig(T_vals, (f1_T, f2_T), H, H_data)
+#     T_fix_fig = create_T_fix_fig(H_vals, (f1_H, f2_H), T, T_data)
 
-    return H_fix_fig, T_fix_fig
+#     return H_fix_fig, T_fix_fig
 
 @app.callback(
     [Output('alpha-scale-slider',      'value'),
@@ -357,11 +357,11 @@ def sync_sliders_with_material(material, store):
 @app.callback(
     Output('param-store', 'data'),
     [Input('material-dropdown', 'value'),
-    Input('alpha-scale-slider',      'value'),
-    Input('chi-scale-slider',        'value'),
-    Input('k-scale-slider',    'value'),
-    Input('m-scale-slider',    'value')],
-    State('param-store',       'data'),
+    Input('alpha-scale-slider', 'value'),
+    Input('chi-scale-slider',   'value'),
+    Input('k-scale-slider',     'value'),
+    Input('m-scale-slider',     'value')],
+    State('param-store',        'data'),
     prevent_initial_call=True,
 )
 def update_params(material, a_k, chi_k, k_k, m_k, store):
@@ -374,151 +374,174 @@ def update_params(material, a_k, chi_k, k_k, m_k, store):
     return store
 
 @app.callback(
-    [Output('phi-graph', 'figure'),
+    [Output('H_fix-graph', 'figure'),
+     Output('T_fix-graph', 'figure'),
+     Output('phi-graph', 'figure'),
      Output('theta-graph', 'figure'),
      Output('yz-graph', 'figure'),
      Output('phi-amplitude-graph', 'figure'),
      Output('theta-amplitude-graph', 'figure'),
      Output('frequency-surface-graph', 'figure')],
-    [Input('param-store', 'data'),
-     Input('H-slider', 'value'),
+    [Input('H-slider', 'value'),
      Input('T-slider', 'value'),
      Input('material-dropdown', 'value'),
-     Input('auto-calc-switch',  'on')],
-    prevent_initial_call=True,
+     Input('auto-calc-switch',  'on'),
+     Input("alpha-scale-slider", "value"),
+     Input("chi-scale-slider", "value"),
+     Input("k-scale-slider", "value"),
+     Input("m-scale-slider", "value"),]
 )
-def update_graphs(store, H, T, material, calc_on):
+def update_graphs(H, T, material, calc_on,
+                 alpha_scale, chi_scale, k_scale, m_scale):
     # Определяем, какой input вызвал callback
     ctx = callback_context
     triggered_inputs = [t['prop_id'] for t in ctx.triggered]
     material_changed = any('material-dropdown' in ti for ti in triggered_inputs)
-    params_changed   = any('param-store' in ti for ti in triggered_inputs)
-    switch_on = any('auto-calc-switch' in ti for ti in triggered_inputs)
-    if not calc_on: raise PreventUpdate
-        
-    p = SimParams(**store[material])
-  
-    h_index = np.abs(H_vals - H).argmin()
+    params_changed   = any(('alpha-scale-slider' in ti or 'chi-scale-slider' in ti
+                            or 'k-scale-slider' in ti or 'm-scale-slider' in ti) for ti in triggered_inputs)
+    switch_on        = any('auto-calc-switch' in ti for ti in triggered_inputs)
     
     # Выбор данных в зависимости от материала
     T_vals = T_vals_1 if material=='1' else T_vals_2
     t_index = np.abs(T_vals - T).argmin()
-    m_val = p.m_scale * (m_array_1 if material=='1' else m_array_2)[t_index]
-    M_val = (M_array_1 if material=='1' else M_array_2)[t_index]
-    chi_val = p.chi_scale * (chi_T(T) if material=='1' else chi_const)
-    K_val = p.k_scale * (K_T(T) if material=='1' else K_const)
-    alpha = p.alpha_scale * (alpha_1 if material=='1' else alpha_2)
-    amplitude_phi_static = phi_amplitude if material=='1' else phi_amplitude_2
-    amplitude_theta_static = theta_amplitude if material=='1' else theta_amplitude_2
-    kappa = m_val / gamma
+    h_index = np.abs(H_vals - H).argmin()
+    m_array   = m_scale * (m_array_1 if material == '1' else m_array_2)
+    K_array   = k_scale * (K_array_1 if material == '1' else K_array_2)
+    chi_array = chi_scale * (chi_array_1 if material == '1' else chi_array_2)
+    m_val = m_array[t_index]
+    K_val = K_array[t_index]
+    chi_val = chi_array[t_index]
 
-    H_mesh = H_mesh_1 if material == '1' else H_mesh_2
-    m_mesh = p.m_scale * (m_mesh_1 if material == '1' else m_mesh_2)
-    K_mesh = p.k_scale * (K_mesh_1 if material == '1' else K_mesh_2)
-    chi_mesh = p.chi_scale * (chi_mesh_1 if material == '1' else chi_mesh_2)
-    freq_array1, freq_array2 = compute_frequencies(H_mesh, m_mesh, chi_mesh, K_mesh, gamma)
-    theor_freqs_GHz = sorted(np.round([freq_array1[t_index, h_index], freq_array2[t_index, h_index]], 1), reverse=True)
+    f1_T, f2_T = compute_frequencies_H_fix(H, m_array, chi_array, K_array, gamma)
+    f1_H, f2_H = compute_frequencies_T_fix(H_vals, m_val, chi_val, K_val, gamma)
+    H_data = H_1000 if H==1000 and material == '1' else None
+    T_data = T_293 if T==293 and material == '1' else None
 
-    sim_time, sol = run_simulation(H, m_val, M_val, K_val, chi_val, alpha, kappa)
-
-    time_ns = sim_time * 1e9
-    theta = np.degrees(sol[0])
-    phi = np.degrees(sol[1])
-
-    # Выполнение аппроксимации
-    if False:
-        A1_theta = np.max(theta) / 2
-        A2_theta = A1_theta
-        A1_phi = np.max(phi) / 2
-        A2_phi = A1_phi
     
-        initial_guess_stage1 = [0, 2, 0, 2, 0, 2, 0, 2, theor_freqs_GHz[0], theor_freqs_GHz[1]]
-        lower_bounds_stage1 = [-np.pi, 0.01, -np.pi, 0.01, -np.pi, 0.01, -np.pi, 0.01, 0.1, 0.1]
-        upper_bounds_stage1 = [np.pi, 100, np.pi, 100, np.pi, 100, np.pi, 100, 120, 120]
-    
-        result_stage1 = least_squares(
-            residuals_stage1,
-            x0=initial_guess_stage1,
-            bounds=(lower_bounds_stage1, upper_bounds_stage1),
-            args=(sim_time, theta, phi, A1_theta, A2_theta, A1_phi, A2_phi),
-            xtol=1e-4, ftol=1e-4, gtol=1e-4, max_nfev=10000
-        )
-        (f1_theta_opt, n1_theta_opt, f2_theta_opt, n2_theta_opt,
-         f1_phi_opt, n1_phi_opt, f2_phi_opt, n2_phi_opt,
-         f1_GHz_opt, f2_GHz_opt) = result_stage1.x
-    
-        initial_guess_stage2 = [A1_theta, A2_theta, A1_phi, A2_phi]
-        result_stage2 = least_squares(
-            residuals_stage2,
-            x0=initial_guess_stage2,
-            args=(sim_time, theta, phi, f1_theta_opt, n1_theta_opt, f2_theta_opt, n2_theta_opt,
-                  f1_phi_opt, n1_phi_opt, f2_phi_opt, n2_phi_opt, f1_GHz_opt, f2_GHz_opt),
-            xtol=1e-4, ftol=1e-4, gtol=1e-4, max_nfev=10000
-        )
-        A1_theta_opt, A2_theta_opt, A1_phi_opt, A2_phi_opt = result_stage2.x
-    
-        initial_guess_stage3 = [
-            A1_theta_opt, f1_theta_opt, n1_theta_opt, A2_theta_opt, f2_theta_opt, n2_theta_opt,
-            A1_phi_opt, f1_phi_opt, n1_phi_opt, A2_phi_opt, f2_phi_opt, n2_phi_opt,
-            f1_GHz_opt, f2_GHz_opt
-        ]
-        lower_bounds_stage3 = [
-            -np.inf, -np.pi, 0.01, -np.inf, -np.pi, 0.01,
-            -np.inf, -np.pi, 0.01, -np.inf, -np.pi, 0.01, 0.1, 0.1
-        ]
-        upper_bounds_stage3 = [
-            np.inf, np.pi, 100, np.inf, np.pi, 100,
-            np.inf, np.pi, 100, np.inf, np.pi, 100, 120, 120
-        ]
-        result_stage3 = least_squares(
-            combined_residuals,
-            x0=initial_guess_stage3,
-            bounds=(lower_bounds_stage3, upper_bounds_stage3),
-            args=(sim_time, theta, phi),
-            xtol=1e-8, ftol=1e-8, gtol=1e-8, max_nfev=10000
-        )
-        opt_params = result_stage3.x
-        (A1_theta_opt, f1_theta_opt, n1_theta_opt, A2_theta_opt, f2_theta_opt, n2_theta_opt,
-         A1_phi_opt, f1_phi_opt, n1_phi_opt, A2_phi_opt, f2_phi_opt, n2_phi_opt,
-         f1_GHz_opt, f2_GHz_opt) = opt_params
-    
-        theta_fit = fit_function_theta(sim_time, A1_theta_opt, f1_theta_opt, n1_theta_opt,
-                                   A2_theta_opt, f2_theta_opt, n2_theta_opt,
-                                   f1_GHz_opt, f2_GHz_opt)
-        phi_fit = fit_function_phi(sim_time, A1_phi_opt, f1_phi_opt, n1_phi_opt,
-                                   A2_phi_opt, f2_phi_opt, n2_phi_opt,
-                                   f1_GHz_opt, f2_GHz_opt)
+    if calc_on:
+        M_val = (M_array_1 if material=='1' else M_array_2)[t_index]
+        alpha = alpha_scale * (alpha_1 if material=='1' else alpha_2)
+        amplitude_phi_static = phi_amplitude if material=='1' else phi_amplitude_2
+        amplitude_theta_static = theta_amplitude if material=='1' else theta_amplitude_2
+        kappa = m_val / gamma
         
-        approx_freqs_GHz = sorted(np.round([f1_GHz_opt, f2_GHz_opt], 1), reverse=True)
+        H_mesh = H_mesh_1 if material == '1' else H_mesh_2
+        m_mesh = m_scale * (m_mesh_1 if material == '1' else m_mesh_2)
+        K_mesh = k_scale * (K_mesh_1 if material == '1' else K_mesh_2)
+        chi_mesh = chi_scale * (chi_mesh_1 if material == '1' else chi_mesh_2)
+        freq_array1, freq_array2 = compute_frequencies(H_mesh, m_mesh, chi_mesh, K_mesh, gamma)
+        theor_freqs_GHz = sorted(np.round([freq_array1[t_index, h_index], freq_array2[t_index, h_index]], 1), reverse=True)
+                     
+        sim_time, sol = run_simulation(H, m_val, M_val, K_val, chi_val, alpha, kappa)
+        time_ns = sim_time * 1e9
+        theta = np.degrees(sol[0])
+        phi = np.degrees(sol[1])
 
-        phi_fig = create_phi_fig(time_ns, phi, phi_fit, H, T, approx_freqs_GHz, theor_freqs_GHz, material)
-        theta_fig = create_theta_fig(time_ns, theta, theta_fit)
-
-    else:
-        approx_freqs_GHz = (None, None)
-        theta_fit = None
-        phi_fit = None
+        # Выполнение аппроксимации
+        if False:
+            A1_theta = np.max(theta) / 2
+            A2_theta = A1_theta
+            A1_phi = np.max(phi) / 2
+            A2_phi = A1_phi
+        
+            initial_guess_stage1 = [0, 2, 0, 2, 0, 2, 0, 2, theor_freqs_GHz[0], theor_freqs_GHz[1]]
+            lower_bounds_stage1 = [-np.pi, 0.01, -np.pi, 0.01, -np.pi, 0.01, -np.pi, 0.01, 0.1, 0.1]
+            upper_bounds_stage1 = [np.pi, 100, np.pi, 100, np.pi, 100, np.pi, 100, 120, 120]
+        
+            result_stage1 = least_squares(
+                residuals_stage1,
+                x0=initial_guess_stage1,
+                bounds=(lower_bounds_stage1, upper_bounds_stage1),
+                args=(sim_time, theta, phi, A1_theta, A2_theta, A1_phi, A2_phi),
+                xtol=1e-4, ftol=1e-4, gtol=1e-4, max_nfev=10000
+            )
+            (f1_theta_opt, n1_theta_opt, f2_theta_opt, n2_theta_opt,
+             f1_phi_opt, n1_phi_opt, f2_phi_opt, n2_phi_opt,
+             f1_GHz_opt, f2_GHz_opt) = result_stage1.x
+        
+            initial_guess_stage2 = [A1_theta, A2_theta, A1_phi, A2_phi]
+            result_stage2 = least_squares(
+                residuals_stage2,
+                x0=initial_guess_stage2,
+                args=(sim_time, theta, phi, f1_theta_opt, n1_theta_opt, f2_theta_opt, n2_theta_opt,
+                      f1_phi_opt, n1_phi_opt, f2_phi_opt, n2_phi_opt, f1_GHz_opt, f2_GHz_opt),
+                xtol=1e-4, ftol=1e-4, gtol=1e-4, max_nfev=10000
+            )
+            A1_theta_opt, A2_theta_opt, A1_phi_opt, A2_phi_opt = result_stage2.x
+        
+            initial_guess_stage3 = [
+                A1_theta_opt, f1_theta_opt, n1_theta_opt, A2_theta_opt, f2_theta_opt, n2_theta_opt,
+                A1_phi_opt, f1_phi_opt, n1_phi_opt, A2_phi_opt, f2_phi_opt, n2_phi_opt,
+                f1_GHz_opt, f2_GHz_opt
+            ]
+            lower_bounds_stage3 = [
+                -np.inf, -np.pi, 0.01, -np.inf, -np.pi, 0.01,
+                -np.inf, -np.pi, 0.01, -np.inf, -np.pi, 0.01, 0.1, 0.1
+            ]
+            upper_bounds_stage3 = [
+                np.inf, np.pi, 100, np.inf, np.pi, 100,
+                np.inf, np.pi, 100, np.inf, np.pi, 100, 120, 120
+            ]
+            result_stage3 = least_squares(
+                combined_residuals,
+                x0=initial_guess_stage3,
+                bounds=(lower_bounds_stage3, upper_bounds_stage3),
+                args=(sim_time, theta, phi),
+                xtol=1e-8, ftol=1e-8, gtol=1e-8, max_nfev=10000
+            )
+            opt_params = result_stage3.x
+            (A1_theta_opt, f1_theta_opt, n1_theta_opt, A2_theta_opt, f2_theta_opt, n2_theta_opt,
+             A1_phi_opt, f1_phi_opt, n1_phi_opt, A2_phi_opt, f2_phi_opt, n2_phi_opt,
+             f1_GHz_opt, f2_GHz_opt) = opt_params
+        
+            theta_fit = fit_function_theta(sim_time, A1_theta_opt, f1_theta_opt, n1_theta_opt,
+                                       A2_theta_opt, f2_theta_opt, n2_theta_opt,
+                                       f1_GHz_opt, f2_GHz_opt)
+            phi_fit = fit_function_phi(sim_time, A1_phi_opt, f1_phi_opt, n1_phi_opt,
+                                       A2_phi_opt, f2_phi_opt, n2_phi_opt,
+                                       f1_GHz_opt, f2_GHz_opt)
+            
+            approx_freqs_GHz = sorted(np.round([f1_GHz_opt, f2_GHz_opt], 1), reverse=True)
+    
+            phi_fig = create_phi_fig(time_ns, phi, phi_fit, H, T, approx_freqs_GHz, theor_freqs_GHz, material)
+            theta_fig = create_theta_fig(time_ns, theta, theta_fit)
+    
+        else:
+            approx_freqs_GHz = (None, None)
+            theta_fit = None
+            phi_fit = None
     
     # Далее строим графики
-    phi_fig = create_phi_fig(time_ns, phi, phi_fit, H, T, approx_freqs_GHz, theor_freqs_GHz, material)
-    theta_fig = create_theta_fig(time_ns, theta, theta_fit)
-    yz_fig = create_yz_fig(np.sin(np.pi/2 + np.radians(theta)) * np.sin(np.radians(phi)),
-                           np.cos(np.pi/2 + np.radians(theta)),
-                           time_ns)
-    if material_changed:
-        phi_amp_fig = create_phi_amp_fig(T_vals, H_vals, amplitude_phi_static)
-        theta_amp_fig = create_theta_amp_fig(T_vals, H_vals, amplitude_theta_static)
-        freq_fig = create_freq_fig(T_vals, H_vals, freq_array1, freq_array2)
-    elif params_changed or switch_on:
-        phi_amp_fig = no_update
-        theta_amp_fig = no_update
-        freq_fig = create_freq_fig(T_vals, H_vals, freq_array1, freq_array2)
+    H_fix_fig = create_H_fix_fig(T_vals, (f1_T, f2_T), H, H_data)
+    T_fix_fig = create_T_fix_fig(H_vals, (f1_H, f2_H), T, T_data)
+                     
+    if calc_on:
+        phi_fig = create_phi_fig(time_ns, phi, phi_fit, H, T, approx_freqs_GHz, theor_freqs_GHz, material)
+        theta_fig = create_theta_fig(time_ns, theta, theta_fit)
+        yz_fig = create_yz_fig(np.sin(np.pi/2 + np.radians(theta)) * np.sin(np.radians(phi)),
+                               np.cos(np.pi/2 + np.radians(theta)),
+                               time_ns)
+        if material_changed:
+            phi_amp_fig = create_phi_amp_fig(T_vals, H_vals, amplitude_phi_static)
+            theta_amp_fig = create_theta_amp_fig(T_vals, H_vals, amplitude_theta_static)
+            freq_fig = create_freq_fig(T_vals, H_vals, freq_array1, freq_array2)
+        elif params_changed or switch_on:
+            phi_amp_fig = no_update
+            theta_amp_fig = no_update
+            freq_fig = create_freq_fig(T_vals, H_vals, freq_array1, freq_array2)
+        else:
+            phi_amp_fig = no_update
+            theta_amp_fig = no_update
+            freq_fig = no_update
     else:
+        phi_fig = no_update
+        theta_fig = no_update
+        yz_fig = no_update
         phi_amp_fig = no_update
         theta_amp_fig = no_update
         freq_fig = no_update
 
-    return phi_fig, theta_fig, yz_fig, phi_amp_fig, theta_amp_fig, freq_fig
+    return H_fix_fig, T_fix_fig, phi_fig, theta_fig, yz_fig, phi_amp_fig, theta_amp_fig, freq_fig
 
 if __name__ == '__main__':
     app.run_server(debug=False, host="0.0.0.0", port=8000)
