06409e6989624fd833ba66e3d8847df7a62252b5
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-07-23 17:13:42 +0300

    Скачиваем теорию

--- app.py
+++ app.py
@@ -183,16 +183,16 @@ app.layout = html.Div([
             html.Button('Скачать данные', id='download-H-btn',
                         style={'margin-bottom': '5px'}),
             dcc.Download(id='download-H-file'),
-            dcc.Graph(id='H_fix-graph',
-                      style={'width': '25%', 'height': 'calc(25vw)'})
-        ], style={'display': 'inline-block', 'verticalAlign': 'top'}),
+            dcc.Graph(id='H_fix-graph'})
+        ], style={'display': 'inline-block', 'verticalAlign': 'top',
+                  'width': '25%', 'height': 'calc(25vw)'}),
         html.Div([
             html.Button('Скачать данные', id='download-T-btn',
                         style={'margin-bottom': '5px'}),
             dcc.Download(id='download-T-file'),
-            dcc.Graph(id='T_fix-graph',
-                      style={'width': '25%', 'height': 'calc(25vw)'})
-        ], style={'display': 'inline-block', 'verticalAlign': 'top'}),
+            dcc.Graph(id='T_fix-graph')
+        ], style={'display': 'inline-block', 'verticalAlign': 'top',
+                  'width': '25%', 'height': 'calc(25vw)'}}),
     ]),
                       
 
