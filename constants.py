4aac752f057ae6c32224e550ec37d317543b88d4
Grigory <44641914+LevkinGI@users.noreply.github.com>
2026-01-16 22:49:33 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -39,11 +39,13 @@ h_IFE = 7500                # Ое
 delta_t = 250e-15           # с
 
 # Материал 1
+@njit(cache=False, fastmath=True)
 def k_T(temperature: Iterable[float] | float) -> np.ndarray:
     """Анизотропия как функция температуры."""
     T = np.asarray(temperature, dtype=float)
     return 0.522 * (T - 370.0) ** 2
 
+@njit(cache=False, fastmath=True)
 def chi_func(m: np.ndarray | float, M: np.ndarray | float) -> np.ndarray | float:
     """Вычисление магнитной восприимчивости."""
     denom = 1.0 - (m**2) / (M**2 + 1e-16)
