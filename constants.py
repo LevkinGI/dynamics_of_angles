c93f038e6e7da83b74094e1741c29802b8204ec5
Grigory <44641914+LevkinGI@users.noreply.github.com>
2026-01-19 18:48:15 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -51,7 +51,7 @@ H_lim = 4000
 H_vals = np.arange(0, H_lim + 1, H_step)
 T_vals_1 = np.linspace(290, 350, 601)
 T_vals_2 = np.linspace(290, 350, 61)
-T_init = 293
+T_init = 298
 
 gamma = 1.76e7              # рад/(с·Oe)
 lam_1 = 12500
