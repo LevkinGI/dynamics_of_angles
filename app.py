f1931b2e80f363043cf18627b6317d72a28fdc47
Grigory <44641914+LevkinGI@users.noreply.github.com>
2026-01-27 16:44:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,10 @@ app.layout = html.Div([
                         zaxis_title='Частота (ГГц)',
                         camera=dict(
                             projection=dict(type='orthographic'),
+                            eye=dict(x=0, z=0),
                         ),
+                        aspectmode='manual',
+                        aspectratio=dict(x=1, y=1, z=0.7),
                     ),
                     font=dict(size=14),
                     template="plotly_white"
