3cadd20adb02928ccd73d080ae40133b420ba7ff
Grigory <44641914+LevkinGI@users.noreply.github.com>
2026-05-19 16:17:39 +0300

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -570,8 +570,8 @@ def create_T_fix_fig(H_vals, T_fix_res, T, data=None, language='eng', theory_ins
                 sign = "−" if slope_gamma < 0 else ""
                 label_text = f"df/dH = {sign}{abs(slope_gamma):.1f}γ"
 
-                label_x_frac = 0.8 if slope >= 0 else 0.58
-                label_yshift = -24 if slope >= 0 else -28
+                label_x_frac = 0.8 if slope >= 0 else 0.5
+                label_yshift = -25 if slope >= 0 else -30
                 x_label = x0 + label_x_frac * (x1 - x0)
                 y_label = slope * x_label + intercept
 
