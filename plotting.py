90f3e0f95c515080ca725d722ab425e4b4c06492
Grigory <44641914+LevkinGI@users.noreply.github.com>
2026-06-01 18:04:00 +0300

    Убрана смена цветов выше Тм

--- plotting.py
+++ plotting.py
@@ -2,6 +2,7 @@
 import plotly.graph_objs as go
 import plotly.colors as pc
 import numpy as np
+from constants import Tm
 
 LF_COLOR = '#e74c3c'
 LF_LIGHT = '#f3c1b7'
@@ -289,10 +290,32 @@ def create_yz_fig(
     return fig
 
 dot_size = 8
-    
+
 def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
     tr = _tr(language)
-    T_plane = 333.0
+    swap_at_Tm = False
+
+    def theory_swap_at_Tm(T_vals, f_hf, f_lf, Tm, enabled=False):
+        """
+        Разделяет теоретические кривые по Tm и при необходимости
+        меняет HF и LF местами в области T > Tm.
+        """
+        mask_lo = T_vals <= Tm
+        mask_hi = T_vals >= Tm
+
+        T_lo, T_hi = T_vals[mask_lo], T_vals[mask_hi]
+
+        hf_lo = f_hf[mask_lo]
+        lf_lo = f_lf[mask_lo]
+
+        if enabled:
+            hf_hi = f_lf[mask_hi]
+            lf_hi = f_hf[mask_hi]
+        else:
+            hf_hi = f_hf[mask_hi]
+            lf_hi = f_lf[mask_hi]
+
+        return T_lo, T_hi, hf_lo, hf_hi, lf_lo, lf_hi
 
     T_vals = np.asarray(T_vals, dtype=float)
     (f1, t1), (f2, t2) = H_fix_res
@@ -304,12 +327,13 @@ def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
     f1 = f1[order]
     f2 = f2[order]
 
-    mask_lo = T_vals <= T_plane
-    mask_hi = T_vals >= T_plane
-
-    T_lo, T_hi = T_vals[mask_lo], T_vals[mask_hi]
-    f1_lo, f1_hi = f1[mask_lo], f1[mask_hi]
-    f2_lo, f2_hi = f2[mask_lo], f2[mask_hi]
+    T_lo, T_hi, f_hf_lo, f_hf_hi, f_lf_lo, f_lf_hi = theory_swap_at_Tm(
+        T_vals=T_vals,
+        f_hf=f1,
+        f_lf=f2,
+        Tm=Tm,
+        enabled=swap_at_Tm
+    )
 
     title_font = dict(family="Times New Roman, Times, serif", size=28, color="black")
     tick_font  = dict(family="Times New Roman, Times, serif", size=24, color="black")
@@ -317,27 +341,27 @@ def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
     fig = go.Figure()
 
     fig.add_vline(
-        x=T_plane,
+        x=Tm,
         line_width=2,
         line_dash="dash",
         line_color="#7f7f7f"
     )
 
     fig.add_trace(go.Scatter(
-        x=T_lo, y=f1_lo, mode='lines', name='HF',
+        x=T_lo, y=f_hf_lo, mode='lines', name='HF',
         line=dict(width=2, color=HF_COLOR)
     ))
     fig.add_trace(go.Scatter(
-        x=T_lo, y=f2_lo, mode='lines', name='LF',
+        x=T_lo, y=f_lf_lo, mode='lines', name='LF',
         line=dict(width=2, color=LF_COLOR)
     ))
 
     fig.add_trace(go.Scatter(
-        x=T_hi, y=f2_hi, mode='lines', name='HF',
+        x=T_hi, y=f_hf_hi, mode='lines', name='HF',
         line=dict(width=2, color=HF_COLOR)
     ))
     fig.add_trace(go.Scatter(
-        x=T_hi, y=f1_hi, mode='lines', name='LF',
+        x=T_hi, y=f_lf_hi, mode='lines', name='LF',
         line=dict(width=2, color=LF_COLOR)
     ))
 
@@ -352,10 +376,12 @@ def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
         y_hf = hf_exp.copy()
         err_y_lf = err_lf_exp.copy()
         err_y_hf = err_hf_exp.copy()
-        m_plane = T_exp > T_plane
+
+        m_plane = T_exp > Tm
         tmp = y_lf[m_plane].copy()
         y_lf[m_plane] = y_hf[m_plane]
         y_hf[m_plane] = tmp
+
         err_tmp = err_y_lf[m_plane].copy()
         err_y_lf[m_plane] = err_y_hf[m_plane]
         err_y_hf[m_plane] = err_tmp
@@ -395,6 +421,7 @@ def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
         tmp = y_lf[m_cross].copy()
         y_lf[m_cross] = y_hf[m_cross]
         y_hf[m_cross] = tmp
+
         err_tmp = err_y_lf[m_cross].copy()
         err_y_lf[m_cross] = err_y_hf[m_cross]
         err_y_hf[m_cross] = err_tmp
@@ -452,7 +479,7 @@ def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
     )
 
     fig.add_annotation(
-        x=T_plane + 1,
+        x=Tm + 1,
         y=0.5,
         xref="x",
         yref="paper",
@@ -464,6 +491,181 @@ def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
     )
 
     return fig
+    
+# def create_H_fix_fig(T_vals, H_fix_res, H, data=None, language='eng'):
+#     tr = _tr(language)
+#     Tm = 333.0
+
+#     T_vals = np.asarray(T_vals, dtype=float)
+#     (f1, t1), (f2, t2) = H_fix_res
+#     f1 = np.asarray(f1, dtype=float)
+#     f2 = np.asarray(f2, dtype=float)
+
+#     order = np.argsort(T_vals)
+#     T_vals = T_vals[order]
+#     f1 = f1[order]
+#     f2 = f2[order]
+
+#     mask_lo = T_vals <= Tm
+#     mask_hi = T_vals >= Tm
+
+#     T_lo, T_hi = T_vals[mask_lo], T_vals[mask_hi]
+#     f1_lo, f1_hi = f1[mask_lo], f1[mask_hi]
+#     f2_lo, f2_hi = f2[mask_lo], f2[mask_hi]
+
+#     title_font = dict(family="Times New Roman, Times, serif", size=28, color="black")
+#     tick_font  = dict(family="Times New Roman, Times, serif", size=24, color="black")
+
+#     fig = go.Figure()
+
+#     fig.add_vline(
+#         x=Tm,
+#         line_width=2,
+#         line_dash="dash",
+#         line_color="#7f7f7f"
+#     )
+
+#     fig.add_trace(go.Scatter(
+#         x=T_lo, y=f1_lo, mode='lines', name='HF',
+#         line=dict(width=2, color=HF_COLOR)
+#     ))
+#     fig.add_trace(go.Scatter(
+#         x=T_lo, y=f2_lo, mode='lines', name='LF',
+#         line=dict(width=2, color=LF_COLOR)
+#     ))
+
+#     fig.add_trace(go.Scatter(
+#         x=T_hi, y=f2_hi, mode='lines', name='HF',
+#         line=dict(width=2, color=HF_COLOR)
+#     ))
+#     fig.add_trace(go.Scatter(
+#         x=T_hi, y=f1_hi, mode='lines', name='LF',
+#         line=dict(width=2, color=LF_COLOR)
+#     ))
+
+#     if data is not None:
+#         T_exp  = np.asarray(data[0], dtype=float)
+#         lf_exp = np.asarray(data[1], dtype=float)
+#         hf_exp = np.asarray(data[2], dtype=float)
+#         err_lf_exp = np.asarray(data[5], dtype=float)
+#         err_hf_exp = np.asarray(data[6], dtype=float)
+
+#         y_lf = lf_exp.copy()
+#         y_hf = hf_exp.copy()
+#         err_y_lf = err_lf_exp.copy()
+#         err_y_hf = err_hf_exp.copy()
+#         m_plane = T_exp > Tm
+#         tmp = y_lf[m_plane].copy()
+#         y_lf[m_plane] = y_hf[m_plane]
+#         y_hf[m_plane] = tmp
+#         err_tmp = err_y_lf[m_plane].copy()
+#         err_y_lf[m_plane] = err_y_hf[m_plane]
+#         err_y_hf[m_plane] = err_tmp
+
+#         diff_th = np.asarray(f1, dtype=float) - np.asarray(f2, dtype=float)
+#         eps0 = 0.0
+#         d = diff_th.copy()
+#         d[np.abs(d) <= eps0] = 0.0
+
+#         cross_T = []
+#         zero_idx = np.where(d == 0.0)[0]
+#         for i in zero_idx:
+#             cross_T.append(float(T_vals[i]))
+
+#         for i in range(len(T_vals) - 1):
+#             d0, d1 = d[i], d[i + 1]
+#             if d0 == 0.0 or d1 == 0.0:
+#                 continue
+#             if d0 * d1 < 0.0:
+#                 T0, T1 = float(T_vals[i]), float(T_vals[i + 1])
+#                 T_cross = T0 - d0 * (T1 - T0) / (d1 - d0)
+#                 cross_T.append(float(T_cross))
+
+#         if cross_T:
+#             cross_T = np.array(sorted(cross_T), dtype=float)
+#             cross_T = cross_T[np.concatenate([[True], np.diff(cross_T) > 1e-9])].tolist()
+
+#         if len(cross_T) >= 2:
+#             T_low, T_high = float(cross_T[0]), float(cross_T[-1])
+#             m_cross = (T_exp >= T_low) & (T_exp <= T_high)
+#         elif len(cross_T) == 1:
+#             T1 = float(cross_T[0])
+#             m_cross = T_exp >= T1
+#         else:
+#             m_cross = np.zeros_like(T_exp, dtype=bool)
+
+#         tmp = y_lf[m_cross].copy()
+#         y_lf[m_cross] = y_hf[m_cross]
+#         y_hf[m_cross] = tmp
+#         err_tmp = err_y_lf[m_cross].copy()
+#         err_y_lf[m_cross] = err_y_hf[m_cross]
+#         err_y_hf[m_cross] = err_tmp
+
+#         fig.add_trace(go.Scatter(
+#             x=T_exp, y=y_lf, mode='markers', name='LF' + tr['experiment_suffix'],
+#             error_y=dict(type="data", array=err_y_lf),
+#             marker=dict(color=LF_COLOR, size=dot_size, line=dict(width=1, color="#000000"))
+#         ))
+#         fig.add_trace(go.Scatter(
+#             x=T_exp, y=y_hf, mode='markers', name='HF' + tr['experiment_suffix'],
+#             error_y=dict(type="data", array=err_y_hf),
+#             marker=dict(color=HF_COLOR, size=dot_size, line=dict(width=1, color="#000000"))
+#         ))
+
+#     fig.update_layout(
+#         template="plotly_white",
+#         font=dict(family="Times New Roman, Times, serif", size=14, color="black"),
+#         margin=dict(l=90, r=20, t=10, b=70),
+#         title=dict(
+#             text=tr['field_title'].format(value=f"{H/1000:g}"),
+#             x=0.5, y=0.98,
+#             xref='paper',
+#             yref='paper',
+#             xanchor='center', yanchor='top',
+#             font=title_font
+#         ),
+#         xaxis=dict(
+#             title=dict(text=tr['temperature_axis'], font=title_font, standoff=16),
+#             tickfont=tick_font,
+#             tickcolor="black",
+#             tickangle=0,
+#             showline=True,
+#             linewidth=1,
+#             linecolor="black",
+#             mirror=True,
+#             showgrid=True,
+#             gridcolor="#cccccc",
+#             gridwidth=1,
+#         ),
+#         yaxis=dict(
+#             title=dict(text=tr['frequency_axis'], font=title_font, standoff=16),
+#             tickfont=tick_font,
+#             tickcolor="black",
+#             tickangle=0,
+#             showline=True,
+#             linewidth=1,
+#             linecolor="black",
+#             mirror=True,
+#             showgrid=True,
+#             gridcolor="#cccccc",
+#             gridwidth=1,
+#         ),
+#         showlegend=False
+#     )
+
+#     fig.add_annotation(
+#         x=Tm + 1,
+#         y=0.5,
+#         xref="x",
+#         yref="paper",
+#         text=r'T<sub>M</sub>',
+#         showarrow=False,
+#         font=dict(family="Times New Roman, Times, serif", size=28, color="black"),
+#         xanchor="left",
+#         yanchor="middle"
+#     )
+
+#     return fig
 
 def create_T_fix_fig(H_vals, T_fix_res, T, data=None, language='eng', theory_inset=False):
     tr = _tr(language)
@@ -693,28 +895,51 @@ def create_T_fix_fig(H_vals, T_fix_res, T, data=None, language='eng', theory_ins
 
 def create_freq_fig(T_vals, H_vals, freq_res_grid, language='eng'):
     tr = _tr(language)
-    T_plane = 333.0
+    swap_at_Tm = False
+
+    def theory_swap_at_Tm(T_vals, f_hf, f_lf, Tm, enabled=False):
+        """
+        Разделяет теоретические поверхности по Tm и при необходимости
+        меняет HF и LF местами в области T > Tm.
+        """
+        mask_lo = T_vals <= Tm
+        mask_hi = T_vals >= Tm
+
+        T_lo, T_hi = T_vals[mask_lo], T_vals[mask_hi]
+
+        hf_lo = f_hf[mask_lo, :]
+        lf_lo = f_lf[mask_lo, :]
+
+        if enabled:
+            hf_hi = f_lf[mask_hi, :]
+            lf_hi = f_hf[mask_hi, :]
+        else:
+            hf_hi = f_hf[mask_hi, :]
+            lf_hi = f_lf[mask_hi, :]
+
+        return T_lo, T_hi, hf_lo, hf_hi, lf_lo, lf_hi
 
     T_vals = np.asarray(T_vals, dtype=float)
     H_kOe  = np.asarray(H_vals, dtype=float) / 1000.0
 
     (f1_grid, _), (f2_grid, _) = freq_res_grid
+    f1_grid = np.asarray(f1_grid, dtype=float)
+    f2_grid = np.asarray(f2_grid, dtype=float)
 
-    mask_lo = T_vals <= T_plane
-    mask_hi = T_vals >= T_plane
-    T_lo = T_vals[mask_lo]
-    T_hi = T_vals[mask_hi]
-    f1_lo = f1_grid[mask_lo, :]
-    f1_hi = f1_grid[mask_hi, :]
-    f2_lo = f2_grid[mask_lo, :]
-    f2_hi = f2_grid[mask_hi, :]
+    T_lo, T_hi, f_hf_lo, f_hf_hi, f_lf_lo, f_lf_hi = theory_swap_at_Tm(
+        T_vals=T_vals,
+        f_hf=f1_grid,
+        f_lf=f2_grid,
+        Tm=Tm,
+        enabled=swap_at_Tm
+    )
 
     zmin = float(np.nanmin([np.nanmin(f1_grid), np.nanmin(f2_grid)]))
     zmax = float(np.nanmax([np.nanmax(f1_grid), np.nanmax(f2_grid)]))
 
     Hmin, Hmax = float(np.nanmin(H_kOe)), float(np.nanmax(H_kOe))
     x_plane = np.array([[Hmin, Hmax], [Hmin, Hmax]])
-    y_plane = np.array([[T_plane, T_plane], [T_plane, T_plane]])
+    y_plane = np.array([[Tm, Tm], [Tm, Tm]])
     z_plane = np.array([[zmin, zmin], [zmax, zmax]])
 
     title_font = dict(family="Times New Roman, Times, serif", size=18, color="black")
@@ -727,7 +952,7 @@ def create_freq_fig(T_vals, H_vals, freq_res_grid, language='eng'):
         colorscale=[[0, PLANE_COLOR], [1, PLANE_COLOR]],
         showscale=False,
         opacity=0.25,
-        name=tr['temperature_title'].format(value=f"{T_plane:g}"),
+        name=tr['temperature_title'].format(value=f"{Tm:g}"),
         hoverinfo="skip"
     ))
 
@@ -735,27 +960,27 @@ def create_freq_fig(T_vals, H_vals, freq_res_grid, language='eng'):
     pad = "<br>" * int(title_pad_lines)
 
     fig.add_trace(go.Surface(
-        z=f1_lo, x=H_kOe, y=T_lo,
+        z=f_hf_lo, x=H_kOe, y=T_lo,
         colorscale=[[0.0, HF_LIGHT], [0.45, HF_MID], [1.0, HF_COLOR]],
         cmin=f1_grid.min(), cmax=f1_grid.max(),
         showscale=False, name='HF'
     ))
     fig.add_trace(go.Surface(
-        z=f2_hi, x=H_kOe, y=T_hi,
+        z=f_hf_hi, x=H_kOe, y=T_hi,
         colorscale=[[0.0, HF_LIGHT], [0.45, HF_MID], [1.0, HF_COLOR]],
-        cmin=f2_grid.min(), cmax=f2_grid.max(),
+        cmin=f1_grid.min(), cmax=f1_grid.max(),
         showscale=False, name='HF'
     ))
     fig.add_trace(go.Surface(
-        z=f2_lo, x=H_kOe, y=T_lo,
+        z=f_lf_lo, x=H_kOe, y=T_lo,
         colorscale=[[0.0, LF_LIGHT], [0.45, LF_MID], [1.0, LF_COLOR]],
         cmin=f2_grid.min(), cmax=f2_grid.max(),
         showscale=False, name='LF'
     ))
     fig.add_trace(go.Surface(
-        z=f1_hi, x=H_kOe, y=T_hi,
+        z=f_lf_hi, x=H_kOe, y=T_hi,
         colorscale=[[0.0, LF_LIGHT], [0.45, LF_MID], [1.0, LF_COLOR]],
-        cmin=f1_grid.min(), cmax=f1_grid.max(),
+        cmin=f2_grid.min(), cmax=f2_grid.max(),
         showscale=False, name='LF'
     ))
 
@@ -791,27 +1016,104 @@ def create_freq_fig(T_vals, H_vals, freq_res_grid, language='eng'):
 
     return fig
 
-# def create_freq_fig(T_vals, H_vals, freq_array1, freq_array2):
-#     fig = go.Figure(
-#         data=[
-#             go.Surface(z=freq_array1, x=H_vals, y=T_vals,
-#                        colorscale=[[0, 'rgb(255, 182, 193)'], [1, 'rgb(255, 0, 0)']],
-#                        showscale=False, name='HF'),
-#             go.Surface(z=freq_array2, x=H_vals, y=T_vals,
-#                        colorscale=[[0, 'rgb(173, 216, 230)'], [1, 'rgb(0, 0, 255)']],
-#                        showscale=False, name='LF')
-#         ],
-#         layout=go.Layout(
-#             title="Частоты LF и HF в зависимости от H и T",
-#             scene=dict(
-#                 xaxis_title='Магнитное поле (Э)',
-#                 yaxis_title='Температура (K)',
-#                 zaxis_title='Частота (ГГц)'
+# def create_freq_fig(T_vals, H_vals, freq_res_grid, language='eng'):
+#     tr = _tr(language)
+#     Tm = 333.0
+
+#     T_vals = np.asarray(T_vals, dtype=float)
+#     H_kOe  = np.asarray(H_vals, dtype=float) / 1000.0
+
+#     (f1_grid, _), (f2_grid, _) = freq_res_grid
+
+#     mask_lo = T_vals <= Tm
+#     mask_hi = T_vals >= Tm
+#     T_lo = T_vals[mask_lo]
+#     T_hi = T_vals[mask_hi]
+#     f1_lo = f1_grid[mask_lo, :]
+#     f1_hi = f1_grid[mask_hi, :]
+#     f2_lo = f2_grid[mask_lo, :]
+#     f2_hi = f2_grid[mask_hi, :]
+
+#     zmin = float(np.nanmin([np.nanmin(f1_grid), np.nanmin(f2_grid)]))
+#     zmax = float(np.nanmax([np.nanmax(f1_grid), np.nanmax(f2_grid)]))
+
+#     Hmin, Hmax = float(np.nanmin(H_kOe)), float(np.nanmax(H_kOe))
+#     x_plane = np.array([[Hmin, Hmax], [Hmin, Hmax]])
+#     y_plane = np.array([[Tm, Tm], [Tm, Tm]])
+#     z_plane = np.array([[zmin, zmin], [zmax, zmax]])
+
+#     title_font = dict(family="Times New Roman, Times, serif", size=18, color="black")
+#     tick_font  = dict(family="Times New Roman, Times, serif", size=14, color="black")
+
+#     fig = go.Figure()
+
+#     fig.add_trace(go.Surface(
+#         x=x_plane, y=y_plane, z=z_plane,
+#         colorscale=[[0, PLANE_COLOR], [1, PLANE_COLOR]],
+#         showscale=False,
+#         opacity=0.25,
+#         name=tr['temperature_title'].format(value=f"{Tm:g}"),
+#         hoverinfo="skip"
+#     ))
+
+#     title_pad_lines = 10
+#     pad = "<br>" * int(title_pad_lines)
+
+#     fig.add_trace(go.Surface(
+#         z=f1_lo, x=H_kOe, y=T_lo,
+#         colorscale=[[0.0, HF_LIGHT], [0.45, HF_MID], [1.0, HF_COLOR]],
+#         cmin=f1_grid.min(), cmax=f1_grid.max(),
+#         showscale=False, name='HF'
+#     ))
+#     fig.add_trace(go.Surface(
+#         z=f2_hi, x=H_kOe, y=T_hi,
+#         colorscale=[[0.0, HF_LIGHT], [0.45, HF_MID], [1.0, HF_COLOR]],
+#         cmin=f2_grid.min(), cmax=f2_grid.max(),
+#         showscale=False, name='HF'
+#     ))
+#     fig.add_trace(go.Surface(
+#         z=f2_lo, x=H_kOe, y=T_lo,
+#         colorscale=[[0.0, LF_LIGHT], [0.45, LF_MID], [1.0, LF_COLOR]],
+#         cmin=f2_grid.min(), cmax=f2_grid.max(),
+#         showscale=False, name='LF'
+#     ))
+#     fig.add_trace(go.Surface(
+#         z=f1_hi, x=H_kOe, y=T_hi,
+#         colorscale=[[0.0, LF_LIGHT], [0.45, LF_MID], [1.0, LF_COLOR]],
+#         cmin=f1_grid.min(), cmax=f1_grid.max(),
+#         showscale=False, name='LF'
+#     ))
+
+#     fig.update_layout(
+#         template="plotly_white",
+#         font=dict(family="Times New Roman, Times, serif", size=14, color="black"),
+#         margin=dict(l=120, r=20, t=10, b=70),
+#         scene=dict(
+#             xaxis=dict(
+#                 title=dict(text=''),  # dict(text=tr['magnetic_field_axis'], font=title_font),
+#                 tickfont=tick_font,
+#                 tickcolor="black",
 #             ),
-#             font=dict(size=14),
-#             template="plotly_white"
+#             yaxis=dict(
+#                 title=dict(text=''),  # dict(text=tr['temperature_axis'], font=title_font),
+#                 tickfont=tick_font,
+#                 tickcolor="black",
+#             ),
+#             zaxis=dict(
+#                 title=dict(text=''),  # dict(text=tr['frequency_axis'], font=title_font),
+#                 tickfont=tick_font,
+#                 tickcolor="black",
+#             ),
+#             camera=dict(projection=dict(type='orthographic')),
+#             aspectmode='manual',
+#             aspectratio=dict(x=1, y=1, z=0.8),
+#         ),
+#         legend=dict(
+#             font=dict(family="Times New Roman, Times, serif", size=12, color="black"),
+#             bgcolor="rgba(255,255,255,0.7)"
 #         )
 #     )
+
 #     return fig
 
 def create_phase_fig(T_vals, theta_0, language='eng'):
